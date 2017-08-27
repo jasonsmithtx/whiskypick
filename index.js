12ddc5d0aaa9cd0c188a52fcfef8b55b407153d1
kevinnayar <kevin.nayar@gmail.com>
2017-08-27 11:02:22 -0500

    adding static.json

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.set('port', process.env.PORT || 3001);
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
