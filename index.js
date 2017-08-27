b297b215122164efb69dec2cc8e8d601e845f9cc
kevinnayar <kevin.nayar@gmail.com>
2017-08-27 17:52:51 -0500

    editing server config

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('port', process.env.PORT || 3001);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+// }
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + 'client/build/index.html'));
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
