b9e073915a7c4a20d3240a2584259656569ff72a
kevinnayar <kevin.nayar@gmail.com>
2017-12-17 11:54:18 -0600

    adding new whiskeys and making header sticky on scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,42 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
+  constructor(props, context) {
+    super (props, context)
+
+    this.state = {
+      makeHeaderSticky: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this))
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll(event) {
+    const supportPageOffset = window.pageXOffset !== undefined
+    const distanceFromTop = supportPageOffset ? window.pageYOffset : document.documentElement.scrollTop
+
+    if (this.state.makeHeaderSticky === false && distanceFromTop >= 100) {
+      this.setState({
+        makeHeaderSticky: !this.state.makeHeaderSticky
+      })
+    }
+
+    if (this.state.makeHeaderSticky === true && distanceFromTop < 100) {
+      this.setState({
+        makeHeaderSticky: !this.state.makeHeaderSticky
+      })
+    }
+  }
+
   render() {
     return (
-      <header className="header">
+      <header className={`${this.state.makeHeaderSticky ? 'sticky header' : 'header'}`}>
         <h1 className="title"><Link to="/">WhiskyPick</Link></h1>
       </header>
     )
--- src/constants/DataSources.js
+++ src/constants/DataSources.js
@@ -1,4 +1,4 @@
-export const DATA_WHISKIES = '//cdn.rawgit.com/kevinnayar/datasets/5bf104a4/whiskey/public/data/whiskies.json'
+export const DATA_WHISKIES = '//cdn.rawgit.com/kevinnayar/datasets/7788d084/whiskey/public/data/whiskies.json'
 export const DATA_FILTERS = '//cdn.rawgit.com/kevinnayar/datasets/b4452499/whiskey/public/data/filters.json'
 export const DATA_PROFILES = '//cdn.rawgit.com/kevinnayar/datasets/b4452499/whiskey/public/data/profiles.json'
 export const DATA_SORTERS = '//cdn.rawgit.com/kevinnayar/datasets/b4452499/whiskey/public/data/sorters.json'
